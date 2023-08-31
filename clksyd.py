3c0d55e34316afbd537f4d7b369a3a1ae72366cf
林夕 <75199709+linxi-520@users.noreply.github.com>
2023-08-31 20:03:25 +0800

    Update clksyd.py

--- clksyd.py
+++ clksyd.py
@@ -43,8 +43,6 @@ headers = {
     'User-Agent':'Mozilla/5.0 (Linux; U; Android 4.1.2; zh-cn; GT-I9300 Build/JZO54K) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30 MicroMessenger/5.2.380'
 }
 
-
-
 checkDict=[
 'Mzg4MDU1MTc0NA==',
 'MzU5MDc0NjU4Mg==',
