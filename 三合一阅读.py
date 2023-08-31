a019b12ca2ebf6ffe327881f794cd6212eef0dee
林夕 <75199709+linxi-520@users.noreply.github.com>
2023-08-31 08:58:42 +0800

    Update 三合一阅读.py

--- 三合一阅读.py
+++ 三合一阅读.py
@@ -1,55 +1,35 @@
 # Author: lindaye
-# update: 2023-08-29 16:00
-# http://mr1692750884645.aidhtjj.cn/coin/index.html?mid=CR42F6WUF 【元宝阅读】
-# http://mr1692750916083.dsxanvq.cn/ox/index.html?mid=RG7UUSYFS 【星空阅读】
-# http://mr1692750963995.stijhqm.cn/user/index.html?mid=D33C7W3A3 【花花阅读】
-# 更新: 1.添加wxpusher备用推送,替换林夕微信推送助手V1.0,无上限人数限制 2.林夕微信推送助手V1.0(关注人数达到上限,老用户不受影响) 
-# wxpusher 使用教程: 扫码获取UID(填写到wxname): https://wxpusher.zjiecode.com/demo/
-# Version:V0.5
+# V1.1.6
+# 2023.8.30更新:
+#   1.改为变量ck,一行一个ck示例
+#   2.采用Wxpusher进行推送服务(手动过检测),仅需扫码获取UID,无需其他操作
+# Wxpusher获取UID: https://wxpusher.zjiecode.com/demo/
+# 变量名 shtoken 示例: {"ck":"这里是cookie中gfsessionid的值","ts":"这里是Wxpusher获取UID"}
 
 import requests
 import re
 import time
-import urllib.parse
+import os
+from urllib.parse import unquote,quote
 
-# Cookie填这
-data = {"un":"##","token":"##","pageSize":20}
+
+ck_token = [eval(line) for line in os.getenv('shtoken').strip().split('\n')]
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Linux; Android 12; RMX3461 Build/RKQ1.210503.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/111.0.5563.116 Mobile Safari/537.36 XWEB/5223 MMWEBSDK/20230701 MMWEBID/7925 MicroMessenger/8.0.40.2420(0x28002851) WeChat/arm64 Weixin NetType/WIFI Language/zh_CN ABI/arm64',
     'Cookie':'',
 }
+
 # 推送域名
 tsurl = 'https://linxi-send.run.goorm.app'
 # 临时用户名
 temp_user = ''
-# WxpusherUID
-wxname = "XX"
+# 微信昵称
+WxpusherUid = ""
 # 保持连接,重复使用
 ss = requests.session()
 
-def test(link):
-    result = ss.post(tsurl+"/task",json={"biz":temp_user,"url":link}).json()
-    WxSend(f"微信阅读-{ydname}阅读", f"{temp_user}-检测文章", "请在60秒内完成当前文章",tsurl+"/read/"+temp_user)
-    check = ''
-    for i in range(30):
-        result = ss.get(tsurl+"/back/"+temp_user).json()
-        if result['status'] == True:
-            check = True 
-            break
-        else:
-            print("等待检测中...", end="\r", flush=True)
-        time.sleep(2)
-    if result['status'] == False:
-        print("手动检测超时,验证失败!")
-        check = False 
-    return check
-
-# 微信推送
-def WxSend(project, status, content,turl):
-    turl = urllib.parse.quote(turl)
-    result = requests.get(f'https://wxpusher.zjiecode.com/demo/send/custom/{wxname}?content={status}-{project}%0A{content}%0A%3Cbody+onload%3D%22window.location.href%3D%27{turl}%27%22%3E').json()
-    print(f"微信消息推送: {result['msg']}")
+data = {"pageSize":20}
 
 
 def user():
@@ -72,13 +52,13 @@ def read():
             if result["result"]["status"] == 10:
                 biz=''.join(re.findall('__biz=(.+)&mid',result["result"]["url"]))
                 if biz in ['Mzg2Mzk3Mjk5NQ==']:
-                    print("检测文章: 请在60秒内完成当前文章")
+                    print("检测文章: 当前为检测文章,请60s内完成验证!")
                     check = test(result["result"]["url"])
                     if check == True:
                         print("检测文章-过检测成功啦!")
-                        time.sleep(5)
+                        time.sleep(10)
                         result = ss.post(domain +'/submit',headers=headers,json=data).json()
-                        print(result)
+                        print(f"阅读成功: 积分{result['result']['val']} 剩余{result['result']['progress']}篇") 
                     else:
                         print("检测文章-过检测失败啦!")
                         break
@@ -117,14 +97,46 @@ def get_money(max_money):
     else:
         print(f"提现失败: {response['msg']}")
 
-for i in ['/user','/coin','/ox']:
+
+def test(link):
+    result = ss.post(tsurl+"/task",json={"biz":temp_user,"url":link}).json()
+    WxSend(f"微信阅读-{ydname}阅读", f"{temp_user}-检测文章", "请在60s内阅读当前文章",tsurl+"/read/"+temp_user)
+    check = ''
+    for i in range(30):
+        result = ss.get(tsurl+"/back/"+temp_user).json()
+        if result['status'] == True:
+            check = True 
+            break
+        else:
+            print("等待检测中...", end="\r", flush=True)
+        time.sleep(2)
+    if result['status'] == False:
+        print("手动检测超时,验证失败!")
+        check = False 
+    return check
+
+
+# 微信推送
+def WxSend(project, status, content,turl):
+    turl = quote(turl)
+    result = requests.get(f'https://wxpusher.zjiecode.com/demo/send/custom/{WxpusherUid}?content={status}-{project}%0A{content}%0A%3Cbody+onload%3D%22window.location.href%3D%27{turl}%27%22%3E').json()
+    print(f"微信消息推送: {result['msg']}")
+    print(f"手动检测链接: {unquote(turl)}")
+
+
+for i in ['/user','/coin','/ox']: # 
     domain = 'http://u.cocozx.cn/api'+i # 花花 /user 元宝阅读 /coin 星空阅读 /ox
     ydlist = {'/user':'花花','/coin':'元宝','/ox':'星空'}
     ydname = ydlist[i]
     print(f"=================={ydname}阅读====================")
-    max_money = user()  # 提现金额
-    read()
-    max_money = user()  # 提现金额
-    time.sleep(1)
-    get_money(max_money)
-    time.sleep(1)
+    for u in ck_token:
+        print(f"============当前第{ck_token.index(u)+1}个账户============")
+        data['un'] = u['un']
+        data['token'] = u['token']
+        WxpusherUid = u['ts']
+        max_money = user()  # 提现金额
+        read()
+        max_money = user()  # 提现金额
+        time.sleep(1)
+        get_money(max_money)
+        time.sleep(1)
