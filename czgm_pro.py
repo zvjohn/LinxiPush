645d8772eceaa3d044d77ab1776a17b56b7da103
林夕 <75199709+linxi-520@users.noreply.github.com>
2023-09-20 22:24:04 +0800

    Update czgm_pro.py

--- czgm_pro.py
+++ czgm_pro.py
@@ -1,10 +1,19 @@
 # Author: lindaye
-# Update:2023-09-15
+# Update:2023-09-20
+# 钢镚(充值购买)阅读
 # 活动入口：http://2496831.sl4mwis5.gbl.avc14qvjzax7.cloud/?p=2496831
-# 变量gbtoken 值{"ck":"gfsessionid的值","ts":"Wxpusher的UID"} 一行一个
-# 内置ck方法ck_token = [{"ck":"gfsessionid的值","ts":"Wxpusher的UID"},{"ck":"gfsessionid的值","ts":"Wxpusher的UID"}]
-# 先扫码关注wxpusher获取UID: https://wxpusher.zjiecode.com/demo/
-# 回调服务器：青龙运行添加LID变量,本地运行修改imei = "xxx(LID值)"为真实设备ID
+# 添加账号说明(青龙/本地)二选一
+#   青龙: 青龙变量gbtoken 值{"ck":"gfsessionid的值","ts":"Wxpusher的UID"} 一行一个(回车分割)
+#   本地: 脚本内置ck方法ck_token = [{"ck":"gfsessionid的值","ts":"Wxpusher的UID"},{"ck":"gfsessionid的值","ts":"Wxpusher的UID"}]
+# 脚本使用说明:
+#   1.(必须操作)扫码关注wxpusher获取UID: https://wxpusher.zjiecode.com/demo/
+#   2.在1打开的网页中点击发送文本消息,查看是否收到,收到可继续
+#   3.将1打开的网页中的UID或者以及操作过1的账号UID复制备用
+#   4.根据提示说明填写账号变量
+# 回调服务器开放说明:
+#   1.仅针对授权用户开放,需配合授权软件使用
+#   2.青龙变量设置LID变量名,值为授权软件的LID
+# 软件版本
 version = "1.1.9"
 import requests
 from multiprocessing import Pool
@@ -24,6 +33,9 @@ Limit = 2
 imei = os.getenv('LID')
 # 充值购买(钢镚)域名(无法使用时请更换)
 domain = 'http://2496831.marskkqh7ij0j.jpsl.u1jcnc75wwbyk.cloud'
+# 保持连接,重复利用
+ss = requests.session()
+
 # 检测文章列表(如有未收录可自行添加)
 check_list = [
     'MzkyMzI5NjgxMA==','MzkzMzI5NjQ3MA==','Mzg5NTU4MzEyNQ==', 'Mzg3NzY5Nzg0NQ==', 'MzU5OTgxNjg1Mg==',
@@ -42,8 +54,6 @@ def get_sign():
 
 # 获取个人信息模块
 def user_info(i,ck):
-    # 保持连接,重复利用
-    ss = requests.session()
     # 请求头
     headers = {
         "User-Agent": "Mozilla/5.0 (Linux; Android 9; V1923A Build/PQ3B.190801.06161913; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/91.0.4472.114 Safari/537.36 MMWEBID/5635 MicroMessenger/8.0.40.2420(0x28002837) WeChat/arm64 Weixin Android Tablet NetType/WIFI Language/zh_CN ABI/arm64",
@@ -55,7 +65,6 @@ def user_info(i,ck):
         userid = share_link.split("=")[1].split("&")[0]
     except:
         print(f"账号【{i+1}】请检查你的CK({ck['ck']})是否正确!")
-        ss.close
         return False
     result = ss.get(domain+"/read/info", headers=headers, data=get_sign()).json()
     if result["code"] == 0:
@@ -64,19 +73,21 @@ def user_info(i,ck):
         print(f"账号【{i+1}】UID:{userid} 余额:{remain} 今日:{read}篇 推广:{share_link}")
     else:
         print(f'账号【{i+1}】用户信息获取失败:{result["message"]}')
-    ss.close
 
 # 阅读文章模块
 def do_read(i,ck):
-    # 保持连接,重复利用
-    ss = requests.session()
     # 请求头
     headers = {
         "User-Agent": "Mozilla/5.0 (Linux; Android 9; V1923A Build/PQ3B.190801.06161913; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/91.0.4472.114 Safari/537.36 MMWEBID/5635 MicroMessenger/8.0.40.2420(0x28002837) WeChat/arm64 Weixin Android Tablet NetType/WIFI Language/zh_CN ABI/arm64",
         "Cookie":f"gfsessionid={ck['ck']};"
     }
     while True:
-        response = ss.get(domain+"/read/task", headers=headers, data=get_sign()).json()
+        response = ss.get(domain+"/read/task", headers=headers, data=get_sign())
+        try:
+            response = response.json()
+        except json.decoder.JSONDecodeError:
+            print(f"账号【{i+1}】请检查你的CK({ck['ck']})是否正确!")
+            return False
         if response["code"] == 1:
             if "秒" in response['message']:
                 print(f"账号【{i+1}】即将开始:{response['message']}")
@@ -126,18 +137,20 @@ def do_read(i,ck):
                 else:
                     print(f"账号【{i+1}】获取文章失败,错误未知{response}")
                     break
-    ss.close
 
 # 提现模块
 def get_money(i,ck):
-     # 保持连接,重复利用
-    ss = requests.session()
     # 请求头
     headers = {
         "User-Agent": "Mozilla/5.0 (Linux; Android 9; V1923A Build/PQ3B.190801.06161913; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/91.0.4472.114 Safari/537.36 MMWEBID/5635 MicroMessenger/8.0.40.2420(0x28002837) WeChat/arm64 Weixin Android Tablet NetType/WIFI Language/zh_CN ABI/arm64",
         "Cookie":f"gfsessionid={ck['ck']};"
     }
-    response = ss.get(domain+"/read/info", headers=headers, data=get_sign()).json()
+    response = ss.get(domain+"/read/info", headers=headers, data=get_sign())
+    try:
+        response = response.json()
+    except json.decoder.JSONDecodeError:
+        print(f"账号【{i+1}】请检查你的CK({ck['ck']})是否正确!")
+        return False
     if response["code"] == 0:
         remain = response["data"]["remain"]
     else:
@@ -152,11 +165,9 @@ def get_money(i,ck):
             print(f'账号【{i+1}】未知错误:{response}')
     else:
         print(f'账号【{i+1}】当前余额为{remain} 未到达2元提现限制!')
-    ss.close
 
 # 微信推送模块
 def check_status(key,link,index):
-    ss = requests.session()
     if ss.get("https://linxi-send.run.goorm.io").status_code ==200:
         callback = "https://linxi-send.run.goorm.io"
     else:
@@ -207,15 +218,15 @@ if __name__ == "__main__":
     else:
         # 本地CK列表
         ck_token = [
-            {"ck":"o-xxxx","ts":"UID_xxxx"}
+            {"ck":"xxxx","ts":"xxxx"}
         ]
         if ck_token == []:
             print('本地变量异常: 请添加本地ck_token示例:{"ck":"xxxx","ts":"UID_xxx"}')
-    print("==================回调验证服务=================")
+    print("==================回调服务器状态=================")
     if imei:
         print(f"[回调服务器]:已启用-[授权ID:{imei}]")
     else:
-        print(f"[回调服务器]:未启用-[变量ID:{imei}]")    
+        print(f"[回调服务器]:未启用-[变量ID:{imei}]")
     # 创建进程池
     with Pool() as pool:
         # 并发执行函数
@@ -225,13 +236,14 @@ if __name__ == "__main__":
         pool.starmap(do_read, list(enumerate(ck_token)))
         print("==================开始账号提现=================")
         pool.starmap(get_money, list(enumerate(ck_token)))
-
+        print("==================开始账号提现=================")
 
         # 关闭进程池
         pool.close()
         # 等待所有子进程执行完毕
         pool.join()
 
+        # 关闭连接
+        ss.close
         # 输出结果
         print(f"================[钢镚阅读V{version}]===============")
-
