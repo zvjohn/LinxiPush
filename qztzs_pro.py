c23a11202f1bc7e1a4d8881f419cf4ad09b88df5
林夕 <75199709+linxi-520@users.noreply.github.com>
2023-09-20 22:24:35 +0800

    Update qztzs_pro.py

--- qztzs_pro.py
+++ qztzs_pro.py
@@ -1,8 +1,13 @@
-# 泉站桶装水小程序
-# author:lindaye
-# 变量示例:{"name":"真实姓名","token":"BVUVVUY"} 一行一个
-# 低保项目(0.2)
+# Author: lindaye
+# Update:2023-09-20
+# 泉站桶装水
+# 活动入口：泉站桶装水小程序
+# 添加账号说明(青龙/本地)二选一
+#   青龙: 青龙变量qztoken 值{"name":"真实姓名","token":"BVUVVUY"} 一行一个(回车分割)
+#   本地: 脚本内置ck方法ck_token = [{"name":"真实姓名","token":"BVUVVUY"},{"name":"真实姓名","token":"BVUVVUY"}]
+# 软件版本
 version = "0.0.2"
+
 import requests
 import json
 import os
@@ -12,10 +17,11 @@ from multiprocessing import Pool
 # 变量类型(本地/青龙)
 Btype = "青龙"
 # 提现限制(元)
-# Limit = 0
+Limit = 1
 # 泉站桶装水域名(无法使用时请更换)
 domain = 'https://admin.dtds888.com/api/index'
-
+# 保持连接,重复利用
+ss = requests.session()
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 MicroMessenger/7.0.20.1781(0x6700143B) NetType/WIFI MiniProgramEnv/Windows WindowsWechat/WMPF XWEB/8379',
     'Content-Type': 'application/json',
@@ -23,7 +29,7 @@ headers = {
 
 def user_info(i,ck):
     data = {"deviceType":"wxapp","timestamp":int(time.time()),"token":ck['token']}
-    result = requests.post(domain + "/index/user_info",headers=headers,json=data).json()
+    result = ss.post(domain + "/index/user_info",headers=headers,json=data).json()
     if result['msg'] == "success":
         print(f"账号【{i+1}】 用户: {result['data']['user']['user_nickname']} 余额: {result['data']['user']['balance']}")
     else:
@@ -31,21 +37,23 @@ def user_info(i,ck):
 
 def do_sign(i,ck):
     data = {"deviceType":"wxapp","timestamp":int(time.time()),"token":ck['token']}
-    result = requests.post(domain + "/user/SignIn",headers=headers,json=data).json()
+    result = ss.post(domain + "/user/SignIn",headers=headers,json=data).json()
     print(f"账号【{i+1}】 签到: {result['msg']} {result['data']}")
 
 
 def get_money(i,ck):
     data = {"deviceType":"wxapp","timestamp":int(time.time()),"token":ck['token']}
-    rmb = requests.post(domain + "/index/user_info",headers=headers,json=data).json()['data']['user']['balance']
-    if rmb >= 1.0:
-        data['money'] = "1"
-        data['name'] = ck['name']
-        result = requests.post(domain+"/user/cashPost",headers=headers,json=data).json()
-        print(f"账号【{i+1}】{result['msg']}")
+    rmb = ss.post(domain + "/index/user_info",headers=headers,json=data).json()['data']
+    if rmb != "":
+        if rmb['user']['balance'] >= Limit:
+            data['money'] = "1"
+            data['name'] = ck['name']
+            result = ss.post(domain+"/user/cashPost",headers=headers,json=data).json()
+            print(f"账号【{i+1}】 {result['msg']}")
+        else:
+            print(f"账号【{i+1}】 账号余额未达到1元提现标准!")
     else:
-        print(f"账号【{i+1}】 账号余额未达到1元提现标准!")
-
+        print(f"账号【{i+1}】账号异常请检查该账号ck是否正确!")
 
 
 
@@ -68,8 +76,7 @@ if __name__ == "__main__":
     else:
         # 本地CK列表
         ck_token = [
-            {"name":"xxx","token":"xxxx=="},
-            {"name":"xxx","token":"xxxx=="}
+            {"name":"真实姓名","token":"BVUVVUY"}
         ]
         if ck_token == []:
             print('账号异常: 请添加本地ck_token示例:{"name":"张三","token":"BVUVVUY"}')
@@ -89,6 +96,8 @@ if __name__ == "__main__":
         pool.close()
         # 等待所有子进程执行完毕
         pool.join()
-
+        
+        # 关闭连接
+        ss.close
         # 输出结果
         print(f"================[泉站桶装水V{version}]===============")
