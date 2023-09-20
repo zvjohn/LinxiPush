f0915de0948fa6b73843d467a9959a1d6d349758
林夕 <75199709+linxi-520@users.noreply.github.com>
2023-09-20 22:26:07 +0800

    Update xzyd_pro.py

--- xzyd_pro.py
+++ xzyd_pro.py
@@ -1,10 +1,13 @@
 # Author: lindaye
+# Update:2023-09-20
 # 小啄阅读
-# 本地和青龙二选一
-# 低保项目0.3/天
-# 跳转到浏览器复制URL,提取里面的readUserid以及sign填入下面uid和sign即可
-# 本地请在脚本最下方添加示例:{"uid":"123456","sign":"92avddsvs545sdvsd1v515s1dv"}到本地变量ck_token
 # 建议一天执行两次(或许有意外之喜)
+# 活动入口：TG内部群
+# 微信打开活动入口跳转到浏览器复制URL,提取里面的readUserid以及sign填入下面uid和sign即可
+# 添加账号说明(青龙/本地)二选一
+#   青龙: 青龙变量qztoken 值{"uid":"123456","sign":"92avddsvs545sdvsd1v515s1dv"} 一行一个(回车分割)
+#   本地: 脚本内置ck方法ck_token = [{"uid":"123456","sign":"92avddsvs545sdvsd1v515s1dv"},{"uid":"123456","sign":"92avddsvs545sdvsd1v515s1dv"}]
+# 软件版本
 version = "0.0.2"
 import requests
 import json
@@ -17,7 +20,8 @@ Btype = "青龙"
 # Limit = 0
 # 小啄阅读域名(无法使用时请更换)
 domain = 'http://redpage230617.read.biwuzhaojin.com'
-
+# 保持连接,重复利用
+ss = requests.session()
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 NetType/WIFI MicroMessenger/7.0.20.1781(0x6700143B) WindowsWechat(0x6309062f) XWEB/8379 Flue',
     'Referer': 'http://sc0909123703.wkyvch9n2.cn/',
@@ -26,34 +30,34 @@ headers = {
 
 def user_info(i,ck):
     data = f'code=null&channelId=230327002&readUserId={ck["uid"]}&sign={ck["sign"]}'
-    result = requests.post(domain+'/web-read/read-info',data=data,headers=headers).json()
+    result = ss.post(domain+'/web-read/read-info',data=data,headers=headers).json()
     if result['code'] == 201:
-        print(f"账号【{str(i+1)}】账号异常请检查该账号ck是否正确!")
+        print(f"账号【{i+1}】账号异常请检查该账号ck是否正确!")
         return False
     else:
-        print(f"账号【{str(i+1)}】钱包余额: {result['data']['walletMoney']}")
+        print(f"账号【{i+1}】钱包余额: {result['data']['walletMoney']}")
 
 
 def do_read(i,ck):
-    print(f"账号【{str(i+1)}】开始阅读")
+    print(f"账号【{i+1}】开始阅读")
     for i in range(30):
         rurl = f'http://red1.read.biwuzhaojin.com/web-read/get-url?readUserId={ck["uid"]}'
-        result = requests.get(rurl).json()
+        result = ss.get(rurl).json()
         if result['code']==200:
             pass
         else:
-            print(f"账号【{str(i+1)}】阅读失败:{result['msg']}")
+            print(f"账号【{i+1}】阅读失败:{result['msg']}")
             break
-    print(f"账号【{str(i+1)}】阅读任务完成")
+    print(f"账号【{i+1}】阅读任务完成")
 
 
 def get_money(i,ck):
     tdata = f'readUserId={ck["uid"]}&sign={ck["sign"]}'
-    result = requests.post(domain+'/web-read/cash-out',data=tdata,headers=headers).json()
+    result = ss.post(domain+'/web-read/cash-out',data=tdata,headers=headers).json()
     if result['code'] == 200:
-        print(f"账号【{str(i+1)}】提现成功 {result['data']['topTitle']}")
+        print(f"账号【{i+1}】提现成功 {result['data']['topTitle']}")
     else:
-        print(f"账号【{str(i+1)}】提现失败 {result['msg']}")
+        print(f"账号【{i+1}】提现失败 {result['msg']}")
 
 
 
@@ -77,9 +81,7 @@ if __name__ == "__main__":
     else:
         # 本地CK列表
         ck_token = [
-            {"uid":"xxxx","sign":"xxxx"},
-            {"uid":"xxxx","sign":"xxxx"},
-            {"uid":"xxxx","sign":"xxxx"},
+            {"uid":"xxxx","sign":"xxxx"}
         ]
         if ck_token == []:
             print('账号异常: 请添加本地ck_token示例:{"uid":"123456","sign":"92avddsvs545sdvsd1v515s1dv"}')
@@ -100,5 +102,7 @@ if __name__ == "__main__":
         # 等待所有子进程执行完毕
         pool.join()
 
+        # 关闭连接
+        ss.close
         # 输出结果
         print(f"================[小啄阅读V{version}]===============")
