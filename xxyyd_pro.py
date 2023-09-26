9aac06feaa312b70101295c84c6205103d76ad4f
林夕 <75199709+linxi-520@users.noreply.github.com>
2023-09-26 09:24:33 +0800

    Update xxyyd_pro.py

--- xxyyd_pro.py
+++ xxyyd_pro.py
@@ -1,16 +1,18 @@
 # Author: lindaye
-# Update:2023-09-20
+# Update:2023-09-26
 # 新小阅阅读
 # 活动入口：https://x.moonbox.site/?BAZZYYha7302#/?recommend=HU3PZX6MGU0
 # 添加账号说明(青龙/本地)二选一
 #   青龙: 青龙变量xxyytoken 值{"ck":"Cookie中app-token"} 一行一个(回车分割)
 #   本地: 脚本内置ck方法ck_token = [{"ck":"Cookie中app-token"},{"ck":"Cookie中app-token"}]
 # 软件版本
-version = "0.0.1"
+version = "0.0.2"
+name = "新小阅阅读"
+linxi_token = "xxyytoken"
+linxi_tips = '{"ck":"Cookie中app-token"}'
 import requests
 import json
 import os
-import time
 from multiprocessing import Pool
 
 # 变量类型(本地/青龙)
@@ -22,7 +24,7 @@ domain = 'https://x.moonbox.site/api'
 # 保持连接,重复利用
 ss = requests.session()
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 NetType/WIFI MicroMessenger/7.0.20.1781(0x6700143B) WindowsWechat(0x6309062f) XWEB/8379 Flue',
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 12; Redmi K30 Pro Build/SKQ1.220303.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/111.0.5563.116 Mobile Safari/537.36 XWEB/5279 MMWEBSDK/20230805 MMWEBID/3850 MicroMessenger/8.0.41.2441(0x28002951) WeChat/arm64 Weixin NetType/4G Language/zh_CN ABI/arm64',
     'Content-Type': 'application/json'
 }
 
@@ -30,7 +32,7 @@ def user_info(i,ck):
     headers['Cookie'] = f'app-token="{ck["ck"]}"'
     result = ss.get(domain+'/user/info',headers=headers).json()
     if result['data'] != None:
-        print(f"账号【{i+1}】用户:{result['data']['nickname']} 余额:{result['data']['balance']}")
+        print(f"账号【{i+1}】用户:{result['data']['nickname']} 余额:{result['data']['balance']}豆")
     else:
         print(f"账号【{i+1}】账号异常请检查该账号ck是否正确!")
         return False
@@ -39,15 +41,14 @@ def user_info(i,ck):
 def do_read(i,ck):
     headers['Cookie'] = f'app-token="{ck["ck"]}"'
     while True:
-        url = "https://x.moonbox.site/api/article/read"
         data = {"articleId": 344,"articleUser": 114,"bigTop": 0,"publishId": 349,"viewNum": "146","readType": 0,"channel": "0","readerDate": 1695174062000,"seconds": 34}
-        result = ss.post(url, headers=headers, json=data).json()
+        result = ss.post(domain+"/article/read", headers=headers, json=data).json()
         if result['code'] == 0:
             print(f"账号【{i+1}】{result['msg']}")
             break
         elif result['code'] == 1:
             if result['data'] != 0:
-                print(f"账号【{i+1}】阅读文章成功-获得[{result['data']}]")
+                print(f"账号【{i+1}】阅读文章成功-获得[{result['data']}]豆")
             else:
                 print(f"账号【{i+1}】账号异常请检查该账号ck是否正确!")
                 return False
@@ -60,7 +61,7 @@ def get_money(i,ck):
         rmb = float(result['data']['balance'])
         result = ss.get(domain+"/account/withdraw/info", headers=headers).json()
         if result['code'] == 1:
-            print(f"账号【{i+1}】余额:{rmb}豆 可提:{result['data']['canWithdrawDou']}豆  冻结:{result['data']['rateDou']}豆")
+            print(f"账号【{i+1}】余额:{rmb}豆 可提:{result['data']['canWithdrawDou']}豆  冻结:{result['data']['rateDou']+result['data']['freezeDou']}豆")
             if result['data']['canWithdrawDou'] >= (Limit*100):
                 url = "https://x.moonbox.site/api/account/cash/withdraw"
                 data = {"dou": result['data']['canWithdrawDou']}
@@ -80,9 +81,6 @@ def get_money(i,ck):
         return False
 
 
-
-
-
 if __name__ == "__main__":
     print(f"""██╗     ██╗███╗   ██╗██╗  ██╗██╗     ██╗  ██╗██╗  ██╗██╗   ██╗██╗   ██╗██████╗ 
 ██║     ██║████╗  ██║╚██╗██╔╝██║     ╚██╗██╔╝╚██╗██╔╝╚██╗ ██╔╝╚██╗ ██╔╝██╔══██╗
@@ -90,22 +88,22 @@ if __name__ == "__main__":
 ██║     ██║██║╚██╗██║ ██╔██╗ ██║╚════╝██╔██╗  ██╔██╗   ╚██╔╝    ╚██╔╝  ██║  ██║
 ███████╗██║██║ ╚████║██╔╝ ██╗██║     ██╔╝ ██╗██╔╝ ██╗   ██║      ██║   ██████╔╝
 ╚══════╝╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝      ╚═╝   ╚═════╝ 
-    项目:新小阅阅读                 BY-林夕               Verion: {version}(并发)
+    项目:{name}           BY-林夕          Verion: {version}(并发)
     Github仓库地址: https://github.com/linxi-520/LinxiPush
 """)
     if Btype == "青龙":
-        if os.getenv('xxyytoken') == None:
-            print('青龙变量异常: 请添加xxyytoken变量示例:{"ck":"xxxx"} 确保一行一个')
+        if os.getenv(linxi_token) == None:
+            print(f'青龙变量异常: 请添加{linxi_token}变量示例:{linxi_tips} 确保一行一个')
             exit()
         # 变量CK列表
-        ck_token = [json.loads(line) for line in os.getenv('xxyytoken').splitlines()]
+        ck_token = [json.loads(line) for line in os.getenv(linxi_token).splitlines()]
     else:
         # 本地CK列表
         ck_token = [
-            {"ck":"xxxx"}
+            # 这里填写本地变量
         ]
         if ck_token == []:
-            print('本地变量异常: 请添加本地ck_token示例:{"ck":"xxxx"}')
+            print(f'本地变量异常: 请添加本地ck_token示例:{linxi_tips}')
     # 创建进程池
     with Pool() as pool:
         # 并发执行函数
@@ -125,4 +123,4 @@ if __name__ == "__main__":
         # 关闭连接
         ss.close
         # 输出结果
-        print(f"================[新小阅阅读V{version}]===============")
+        print(f"================[{name}V{version}]===============")
